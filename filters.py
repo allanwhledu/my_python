74347d8493865d6741c222d55e88933b39f43e8e
Wang Haili <bitwanghaili@gmail.com>
2019-11-24 13:11:54 +0800

    增加高频信号的显示

--- filters.py
+++ filters.py
@@ -8,11 +8,15 @@ t = arange(nsamples) / sample_rate
 x = cos(2*pi*0.5*t)+ 0.2*sin(2*pi*15.3*t)
 
 b, a = signal.butter(8, 0.1, 'lowpass')  # 配置滤波器 8 表示滤波器的阶数
-filtedData = signal.filtfilt(b, a, x)  # data为要过滤的信号
+# c, d = signal.butter(8, 0.8, 'highpass')
+filtedData_l = signal.filtfilt(b, a, x)  # data为要过滤的信号
+# filtedData_h = signal.filtfilt(c, d, x)
 
 fig = figure()
-ax1 = subplot(2,1,1)
-ax2 = subplot(2,1,2)
+ax1 = subplot(3,1,1)
+ax2 = subplot(3,1,2)
+ax3 = subplot(3,1,3)
 ax1.plot(t, x, color='blue')
-ax2.plot(t, filtedData, color='red')
+ax2.plot(t, filtedData_l, color='red')
+ax3.plot(t, x - filtedData_l, color='red')
 show()
